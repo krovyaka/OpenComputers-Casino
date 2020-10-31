bd8fbca57bf8cb11482ca5d21cda34d24d7ae434
krovyaka <krovyaka1@yandex.ru>
2020-10-31 14:18:18 +0300

    renamed repository

--- config/settings.lua
+++ config/settings.lua
@@ -1,6 +1,6 @@
 local settings = {}
 
-settings.REPOSITORY = "https://raw.githubusercontent.com/krovyaka/OpenComputers-Casino-NoVirtual/master"
+settings.REPOSITORY = "https://raw.githubusercontent.com/krovyaka/OpenComputers-Casino/master"
 settings.TITLE = "Приветствуем ваc у нас в казино"
 settings.ADMINS = { "krovyak", "Durex77" }
 
--- install.lua
+++ install.lua
@@ -1,4 +1,4 @@
-local REPOSITOTY = "https://raw.githubusercontent.com/krovyaka/OpenComputers-Casino-NoVirtual/master"
+local REPOSITOTY = "https://raw.githubusercontent.com/krovyaka/OpenComputers-Casino/master"
 
 local shell = require("shell")
 shell.execute("wget -fq " .. REPOSITOTY .. "/launcher.lua /home/1.lua")
