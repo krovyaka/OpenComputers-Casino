ca79cd9a53261b8dbe10be6e8018cd4d6df104a4
Salim <salimabdul.it@gmail.com>
2020-12-11 19:49:16 +0200

    Temporally change the link to feature branch

--- config/settings.lua
+++ config/settings.lua
@@ -1,6 +1,6 @@
 local settings = {}
 
-settings.REPOSITORY = "http://durex77.pythonanywhere.com/krov/proxy?url=https://raw.githubusercontent.com/krovyaka/OpenComputers-Casino/master"
+settings.REPOSITORY = "http://durex77.pythonanywhere.com/krov/proxy?url=https://raw.githubusercontent.com/krovyaka/OpenComputers-Casino/feature/NewGame/OpenChest/"
 settings.TITLE = "Приветствуем ваc у нас в казино"
 settings.ADMINS = { "krovyak", "Durex77" }
 
