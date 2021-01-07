3693b63ade0565946e878e2b0e3628a64d00d0c5
krovyaka <krovyaka1@yandex.ru>
2021-01-08 01:34:46 +0300

    reworked currency displaying

--- config/currencies.lua
+++ config/currencies.lua
@@ -1,66 +1,23 @@
 local currencies = {}
 
-table.insert(currencies, {
-    name = "Деньги",
-    color = 0x85BB65,
-    id = "customnpcs:npcMoney",
-    dmg = 0
-})
+local function addCurrency(name, id, dmg, model, color, max)
+    table.insert(currencies, {
+        name = name,
+        id = id,
+        dmg = dmg,
+        model = model,
+        color = color,
+    })
+end
 
-table.insert(currencies, {
-    name = "Светопыль",
-    color = 0xD0D000,
-    id = "minecraft:glowstone_dust",
-    dmg = 0,
-    max = 50
-})
-
-table.insert(currencies, {
-    name = "Железный слиток",
-    color = 0xAAAAAA,
-    id = "minecraft:iron_ingot",
-    dmg = 0,
-})
-
-table.insert(currencies, {
-    name = "Железный блок",
-    color = 0xAAAAAA,
-    id = "minecraft:iron_block",
-    dmg = 0,
-    max = 64
-})
-
-table.insert(currencies, {
-    name = "Медный слиток",
-    color = 0xA5642F,
-    id = "IC2:itemIngot",
-    dmg = 0
-})
-table.insert(currencies, {
-    name = "Медный блок",
-    color = 0xA5642F,
-    id = "Forestry:resourceStorage",
-    dmg = 1,
-    max = 64
-})
-
-table.insert(currencies, {
-    name = "Оловянный слиток",
-    color = 0xCCCCCC,
-    id = "IC2:itemIngot",
-    dmg = 1
-})
-table.insert(currencies, {
-    name = "Оловянный блок",
-    color = 0xCCCCCC,
-    id = "Forestry:resourceStorage",
-    dmg = 2,
-    max = 64
-})
-
-table.insert(currencies, {
-    name = "Бесплатно",
-    color = 0xE5E5E5,
-})
+addCurrency("Деньги",           "customnpcs:npcMoney",      0,   'INGOT', 0x85BB65, nil)
+addCurrency("Светопыль",        "minecraft:glowstone_dust", 0,   'DUST',  0xD0D000, 5)
+addCurrency("Железный слиток",  "minecraft:iron_ingot",     0,   'INGOT', 0xAAAAAA, nil)
+addCurrency("Железный блок",    "minecraft:iron_block",     0,   'BLOCK', 0xAAAAAA, 6)
+addCurrency("Медный слиток",    "IC2:itemIngot",            0,   'INGOT', 0xA5642F, nil)
+addCurrency("Медный блок",      "Forestry:resourceStorage", 1,   'BLOCK', 0xA5642F, 6)
+addCurrency("Оловянный слиток", "IC2:itemIngot",            1,   'INGOT', 0xCCCCCC, nil)
+addCurrency("Оловянный блок",   "Forestry:resourceStorage", 2,   'BLOCK', 0xCCCCCC, 6)
+addCurrency("Бесплатно",        nil,                        nil, nil,     0xE5E5E5, nil)
 
 return currencies
--- launcher.lua
+++ launcher.lua
@@ -8,6 +8,7 @@ local games
 local currencies
 local image
 local buffer
+local colorlib
 
 REPOSITORY = settings.REPOSITORY
 
@@ -107,10 +108,30 @@ end
 
 local function drawCurrency(x, y, currency, current)
     buffer.drawRectangle(x, y, 46, 3, --[[current and 0xA890AA or--]] 0xE3E3E3, 0, " ")
-    buffer.drawText(x + 8, y    , 0, "Валюта: " .. currency.name)
+    buffer.drawText(x + 8, y    , 0, currency.name)
     buffer.drawText(x + 8, y + 1, 0, "Максимальная ставка: " .. (currency.max or "-"))
-    buffer.drawText(x + 8, y + 2, 0, "Имеется у казино: " .. casino.getCurrencyInStorage(currency))
-    buffer.drawRectangle(x, y, 6, 3, currency.color or 0, 0xE3E3E3, ' ')
+    buffer.drawText(x + 8, y + 2, 0, "У казино: " .. casino.getCurrencyInStorage(currency) + " шт.")
+
+    local color = currency.color or 0xE3E3E3
+    local darkColor = colorlib.transition(color, 0, 0.1)
+    if currency.model == 'INGOT' then
+        buffer.drawSemiPixelLine(x, y * 2 + 1, x + 2, y * 2 + 1, darkColor)
+        buffer.drawSemiPixelLine(x + 3, y * 2, x + 5, y * 2, darkColor)
+        buffer.drawSemiPixelLine(x, y * 2 + 2, x + 2, y * 2 + 2, color)
+        buffer.drawSemiPixelLine(x + 3, y * 2 + 1, x + 5, y * 2 + 1, color)
+        buffer.drawSemiPixelLine(x, y * 2 + 3, x + 2, y * 2 + 3, darkColor)
+        buffer.drawSemiPixelLine(x + 3, y * 2 + 2, x + 5, y * 2 + 2, darkColor)
+    elseif currency.model == 'DUST' then
+        buffer.drawSemiPixelRectangle(x + 2, y * 2 + 1, 3, 2, color)
+        buffer.drawSemiPixelRectangle(x + 1, y * 2 + 3, 5, 1, color)
+        buffer.drawSemiPixelLine(x, y * 2 + 3, x + 3, y * 2, darkColor)
+        buffer.drawSemiPixelLine(x + 3, y * 2, x + 6, y * 2 + 3, darkColor)
+        buffer.drawSemiPixelLine(x + 1, y * 2 + 4, x + 5, y * 2 + 4, darkColor)
+    elseif currency.model == 'BLOCK' then
+        buffer.drawSemiPixelRectangle(x, y * 2 - 1, 6, 6, darkColor)
+        buffer.drawSemiPixelRectangle(x + 1, y * 2, 4, 4, color)
+    end
+
 end
 
 local function drawStatic()
@@ -232,6 +253,7 @@ local function initLauncher()
     currencies = require("currencies")
     image = require("image")
     buffer = require("doubleBuffering")
+    colorlib = require("color")
     casino.setCurrency(currencies[1])
 end
 
